d592a95eebf5a4d085ef6c62ea12669f452679db
ticTpoc <86444430+ticTpoc@users.noreply.github.com>
2021-11-18 09:44:02 -0300

    Update addusuario.php

--- addusuario.php
+++ addusuario.php
@@ -23,7 +23,7 @@ $currentPage= $_SERVER['SCRIPT_NAME'];
      $feito = $_GET['feito'];
 
      if($feito == 0){
-         include_once "forms/".substr($currentPage,15,-4).".php";
+         include_once "forms/".substr($currentPage,26,-4).".php";
      }else{
      
        
